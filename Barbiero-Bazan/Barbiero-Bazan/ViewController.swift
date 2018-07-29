3a53406343c92379127c336c15960ac8a2a1e5bd
giovanni-barbiero <giovanni-barbiero@bitbucket.org>
2018-07-29 18:40:50 +0200

    Project Init

---
+++ Barbiero-Bazan/Barbiero-Bazan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Barbiero-Bazan
+//
+//  Created by Giovanni Barbiero on 29/07/18.
+//  Copyright © 2018 Giovanni Barbiero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
